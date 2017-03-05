9c516c3e0a4cdd393c6042f7c28751790b9567e8
sleeplessmind <eng.ahmed.abdraboh@gmail.com>
2017-03-05 13:43:09 +0200

    edit position of functions

--- DBMSProject.sh
+++ DBMSProject.sh
@@ -1,41 +1,7 @@
 #!/bin/bash
 
 #************************** database management ********************************
-function createDB(){
- 	printf "Enter Database Name: "
-	read dbName
-	mkdir $dbName
-}
 
-function renameDB(){
-	printf "Enter Old Name: "
-	read dbName
-	printf "Enter New Name: "
-	read dbNewName
-	mv  $dbName $dbNewName
-}
-
-function removeDB(){
-     printf "Enter Database Name: "
-     read dbName
-     rm -r $dbName
-}
-function showDB() {
-  ls -d */;
-}
-function useDB(){
-  printf "Enter Database Name: "
-  read dbName
-  cd $dbName
-}
-function showTables(){
-  ls -p | grep -v /
-}
-function createTable(){
-  printf "Enter Table Name: "
-  read tableName
-  touch $tableName
-}
 names='Create-Database Rename-Database Drop-Database Use-Database Show-Databases Quit'
 PS3='Enter Choice Number: '
 namesTables='Create-Table Show-Tables Quit';
@@ -83,3 +49,39 @@ do
                ;;
      esac
 done
+
+function createDB(){
+ 	printf "Enter Database Name: "
+	read dbName
+	mkdir $dbName
+}
+
+function renameDB(){
+	printf "Enter Old Name: "
+	read dbName
+	printf "Enter New Name: "
+	read dbNewName
+	mv  $dbName $dbNewName
+}
+
+function removeDB(){
+     printf "Enter Database Name: "
+     read dbName
+     rm -r $dbName
+}
+function showDB() {
+  ls -d */;
+}
+function useDB(){
+  printf "Enter Database Name: "
+  read dbName
+  cd $dbName
+}
+function showTables(){
+  ls -p | grep -v /
+}
+function createTable(){
+  printf "Enter Table Name: "
+  read tableName
+  touch $tableName
+}
