83f4e31678bbca4837a080d4dfb7b6b83d64fc90
sleeplessmind <eng.ahmed.abdraboh@gmail.com>
2017-03-07 17:51:10 +0200

    create Table fun

--- DBMSProject.sh
+++ DBMSProject.sh
@@ -1,4 +1,81 @@
 #!/bin/bash
+function createDB(){
+ 	printf "Enter Database Name: "
+	read dbName
+	mkdir $dbName
+}
+
+function renameDB(){
+	printf "Enter Old Name: "
+	read dbName
+	printf "Enter New Name: "
+	read dbNewName
+	mv  $dbName $dbNewName
+}
+
+function removeDB(){
+     printf "Enter Database Name: "
+     read dbName
+     rm -r $dbName
+}
+function showDB() {
+  ls -d */;
+}
+function useDB(){
+  printf "Enter Database Name: "
+  read dbName
+  cd $dbName
+}
+function showTables(){
+  ls -p | grep -v /
+}
+function createTable(){
+  declare -a names
+  declare -a types
+  choices='integer string'
+  printf "Enter Table Name: "
+  read tableName
+  printf "Enter Number of Columns: "
+  read numberOfColumns
+  for (( i = 0; i < $numberOfColumns; i++ ));
+  do
+    echo "Enter Name of Column Number $i: "
+    read nameOfColumn
+    names[$i]=$nameOfColumn
+    while true
+    do
+      printf "Enter Type of Column int or string $i: "
+      read Type1
+      if [ $Type1 == "int" ] || [ $Type1 == 'string' ]; then
+        types[$i]=$Type1
+        break
+      else
+        echo 'wrong input'
+      fi
+    done
+  done
+  touch $tableName
+
+  for (( i = 0; i < $numberOfColumns; i++ )); do
+    if [[ i -eq numberOfColumns-1 ]]; then
+      echo -n ${names[$i]}-${types[$i]}>>$tableName
+    else
+      echo -n ${names[$i]}-${types[$i]}:>>$tableName
+    fi
+  done
+}
+# function selectFromTable(){
+#
+# }
+# function updateTable(){
+#
+# }
+# function deleteTable(){
+#
+# }
+# function dropTable(){
+#
+# }
 
 #************************** database management ********************************
 
@@ -61,51 +138,3 @@ do
                ;;
      esac
 done
-
-function createDB(){
- 	printf "Enter Database Name: "
-	read dbName
-	mkdir $dbName
-}
-
-function renameDB(){
-	printf "Enter Old Name: "
-	read dbName
-	printf "Enter New Name: "
-	read dbNewName
-	mv  $dbName $dbNewName
-}
-
-function removeDB(){
-     printf "Enter Database Name: "
-     read dbName
-     rm -r $dbName
-}
-function showDB() {
-  ls -d */;
-}
-function useDB(){
-  printf "Enter Database Name: "
-  read dbName
-  cd $dbName
-}
-function showTables(){
-  ls -p | grep -v /
-}
-function createTable(){
-  printf "Enter Table Name: "
-  read tableName
-  touch $tableName
-}
-function selectFromTable(){
-
-}
-function updateTable(){
-
-}
-function deleteTable(){
-
-}
-function dropTable(){
-
-}
